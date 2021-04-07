468e1dbdaae1d4df47092a0aeedc003442be1b8d
André Davis <CubicleJockey@users.noreply.github.com>
2021-04-06 18:22:52 -0700

    Removing appsettings and setting up UserSecrets and Azure App Configurations

--- MsBlazorServerPlayGround/MsBlazorServerPlayGround/Program.cs
+++ MsBlazorServerPlayGround/MsBlazorServerPlayGround/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using static System.Console;
 
 namespace MsBlazorServerPlayGround
 {
@@ -18,6 +16,30 @@ namespace MsBlazorServerPlayGround
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((host, configuration) =>
+                {
+                    //Added before AddUserSecrets or AzureAppConfiguration to let user secrets override environment variables
+                    configuration.AddEnvironmentVariables();
+
+                    var environment = host.HostingEnvironment;
+                    if (environment.IsEnvironment("Local"))
+                    {
+                        configuration.AddUserSecrets<Program>();
+                    }
+                    else //Develop, QA, & Production Azure App Configurations
+                    {
+                        const string AZURE_APP_CONFIGURATION_ENVIRONMENT_VARIABLE = "ASPNETCORE_CONFIGURATION";
+                        var appConfigurationEndpoint = Environment.GetEnvironmentVariable(AZURE_APP_CONFIGURATION_ENVIRONMENT_VARIABLE);
+                        if (!string.IsNullOrWhiteSpace(appConfigurationEndpoint))
+                        {
+                            configuration.AddAzureAppConfiguration(options => options.Connect(appConfigurationEndpoint));
+                        }
+                        else
+                        {
+                            Error.WriteLine($"Failed to load Azure App Configurations for environment [{environment.EnvironmentName}]. Environment Variable {AZURE_APP_CONFIGURATION_ENVIRONMENT_VARIABLE} is empty.");
+                        }
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
