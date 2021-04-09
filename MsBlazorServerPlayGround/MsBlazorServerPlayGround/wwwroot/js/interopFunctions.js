78fa5c59bac149da4536b1fb29004c382f1fa5b6
André Davis <CubicleJockey@users.noreply.github.com>
2021-04-09 14:56:02 -0700

    More DotNet and JS interactions

---
+++ MsBlazorServerPlayGround/MsBlazorServerPlayGround/wwwroot/js/interopFunctions.js
@@ -0,0 +1,14 @@
+﻿window.dotnetInJs = {
+    getIntegers: function () {
+        DotNet.invokeMethodAsync('MsBlazorServerPlayGround', 'GetListOfIntegers')
+            .then(data => {
+                document.getElementById('integers').innerText = data.join(', ');
+            });
+    },
+    getSentenceAlias: function () {
+        DotNet.invokeMethodAsync('MsBlazorServerPlayGround', 'GimmeASentence')
+            .then(data => {
+                document.getElementById('sentence-csharp-alias').innerText = data;
+            });
+    }
+};
