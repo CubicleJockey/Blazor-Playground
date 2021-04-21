7b231eaa858ce6e40848b1a3df3aa80002c61da4
André Davis <CubicleJockey@users.noreply.github.com>
2021-04-20 17:17:08 -0700

    Finished prep for state management.

---
+++ MsBlazorServerPlayGround/MsBlazorServerPlayGround/Data/StateManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MsBlazorServerPlayGround.Data
+{
+    public class StateManager
+    {
+        private readonly IDictionary<string, object> states;
+
+        public event Action OnChange;
+
+        public StateManager()
+        {
+            states = new Dictionary<string, object>();
+        }
+
+        public void SetState(string key, object value)
+        {
+            if (states.ContainsKey(key))
+            {
+                states[key] = value;
+                NotifyStateChanged();
+            }
+            else
+            {
+                states.Add(key, value);
+                NotifyStateChanged();
+            }
+        }
+
+        public T GetState<T>(string key)
+        {
+            if (!states.ContainsKey(key)) { return default; }
+            return (T)states[key];
+        }
+
+        public (bool removed, T itemRemoved) DeleteState<T>(string key)
+        {
+            if (!states.ContainsKey(key))
+            {
+                return (false, default);
+            }
+
+            var removed = states.Remove(key, out var item);
+            if(removed) { NotifyStateChanged(); }
+
+            return (removed, (T)item ?? default);
+        }
+
+        #region Helper Methods
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        #endregion Helper Methods
+    }
+}
