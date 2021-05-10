73079d3901453837a9e412d7ac9bc08ca163d727
André Davis <CubicleJockey@users.noreply.github.com>
2021-05-10 16:28:24 -0700

    Adding more test examples.
    Note: Need to figure out why .Click and TriggerEventAsync cannot activate a click on a button with onclick as attribute

---
+++ MsBlazorServerPlayGround/MSBlazorServerPlayground.Tests/Pages/JsAndDotNet/DotNetInJsRazorTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bunit;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsBlazorServerPlayGround.Pages.JsAndDotNet;
+using TestContext = Bunit.TestContext;
+
+namespace MSBlazorServerPlayground.Tests.Pages.JsAndDotNet
+{
+    [TestClass]
+    public class DotNetInJsRazorTests
+    {
+        [TestMethod]
+        public async Task JsInvoke()
+        {
+            //Arrange
+            using var testContext = new TestContext();
+            var component = testContext.RenderComponent<DotNetInJs>();
+
+            var button = component.Find("#js-invoke-button");
+            var paragraph = component.Find("#integers");
+
+
+            //Act & Assert
+
+            var text = paragraph.TextContent;
+            text.MarkupMatches(string.Empty);
+
+            await button.TriggerEventAsync("click", EventArgs.Empty);
+
+            text = paragraph.TextContent;
+            text.Should().NotBeNullOrWhiteSpace();
+
+            var numbers = text.Split(',');
+            foreach (var number in numbers)
+            {
+                var isNumber = int.TryParse(number,out _);
+                if(!isNumber) { Assert.Fail("Numbers must be returned."); }
+            }
+        }
+    }
+}
