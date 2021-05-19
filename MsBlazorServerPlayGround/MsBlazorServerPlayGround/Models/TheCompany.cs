61e9e065eec628019608f4c5e7c2840cae61a72a
André Davis <CubicleJockey@users.noreply.github.com>
2021-05-19 12:06:44 -0700

    Lunch Time
    More setup for Generic lists in Components with RenderFragments

--- MsBlazorServerPlayGround/MsBlazorServerPlayGround/Models/TheCompany.cs
+++ MsBlazorServerPlayGround/MsBlazorServerPlayGround/Models/TheCompany.cs
@@ -1,11 +1,12 @@
 ﻿namespace MsBlazorServerPlayGround.Models
 {
-    public class Company
+    public class TheCompany
     {
         public string Name { get; set; }
         public string Suffix { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
+        public string Zip { get; set; }
     }
 }
