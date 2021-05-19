4ba7ebeec5544ebdc070f7a0139ea6c51ef42c0f
André Davis <CubicleJockey@users.noreply.github.com>
2021-05-19 11:57:35 -0700

    Generic Lists
    Setting up for generic list/items

---
+++ MsBlazorServerPlayGround/MsBlazorServerPlayGround/Models/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace MsBlazorServerPlayGround.Models
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Suffix { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
